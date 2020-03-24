2f05d86f97d8ac36f2d4717c8ee3462bf20a1df8
dabal <dabrowski.grzegorz@o2.pl>
2020-03-25 00:41:37 +0100

    poczatek obiektowosci

--- js/app.js
+++ js/app.js
@@ -1,9 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
-    buildMemo(5, 10);
+    let memo = new Memo(5, 10);
+    memo.buildMemo();
 });
 
+var Memo = function(rows, columns) {
+    this.rows = rows;
+    this.columns = columns
+}
 
+Memo.prototype.onMemoCardClick = function(event) {
+    event.target.querySelector(".hidden").style.visibility = "visible";
+    console.log(event.target.querySelector(".hidden"))
+}
 
+Memo.prototype.generateMemoCardDiv = function(klasa, url, id) {
+    newDiv = document.createElement("div");
+    newDiv.classList.add(klasa);
+    if (klasa == "memoItem") {
+        newDiv.dataset.url = url;
+        newDiv.id = id;
+        newDiv.addEventListener("click", this.onMemoCardClick);
+    }
+    newDiv.appendChild(this.generateImgForDiv("http://icons.iconarchive.com/icons/dapino/girl-in-a-bunny-suit/256/girl-bunny-question-icon.png"));
+    return newDiv;
+}
+
+Memo.prototype.generateImgForDiv = function(url) {
+    img = document.createElement("img");
+    img.src = url;
+    img.classList.add("hidden");
+    return img;
+}
+
+Memo.prototype.buildMemo = function() {
+    memo = document.querySelector("div.memo");
+    for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.columns; j++) {
+            memo.appendChild(this.generateMemoCardDiv("memoItem", "tekst", "memo_" + i + "_" + j));
+        }
+        memo.appendChild(this.generateMemoCardDiv("empty", ""));
+    }
+}
 
 function generateArrayOfPairsInRandomOrder(numberOfUniqueElements) {
     let left = generateArrayOfUniqueNumbers(numberOfUniqueElements);
@@ -19,43 +56,3 @@ function generateArrayOfUniqueNumbers(numberOfElements) {
     }
     return arr;
 }
-
-function onMemoCardClick(event) {
-    event.target.querySelector(".hidden").style.visibility = "visible";
-    console.log(event.target.querySelector(".hidden"))
-}
-
-function generateMemoCardDiv(klasa, url, id) {
-    newDiv = document.createElement("div");
-    newDiv.classList.add(klasa);
-    if (klasa == "memoItem") {
-        newDiv.dataset.url = url;
-        newDiv.id = id;
-        newDiv.addEventListener("click", onMemoCardClick);
-    }
-    newDiv.appendChild(generateImgForDiv("http://icons.iconarchive.com/icons/dapino/girl-in-a-bunny-suit/256/girl-bunny-question-icon.png"));
-    return newDiv;
-}
-
-
-
-function generateImgForDiv(url) {
-    img = document.createElement("img");
-    img.src = url;
-    img.classList.add("hidden");
-    return img;
-}
-
-function buildMemo(rows, columns) {
-    memo = document.querySelector("div.memo");
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            memo.appendChild(generateMemoCardDiv("memoItem", "tekst", "memo_" + i + "_" + j));
-        }
-        memo.appendChild(generateMemoCardDiv("empty", ""));
-    }
-}
-
-
-
-//buildMemo(10, 10)
