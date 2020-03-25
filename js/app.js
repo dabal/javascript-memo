d6a62fcf46159c3c5ae6a9c2d32b641677e327b0
dabal <dabrowski.grzegorz@o2.pl>
2020-03-25 02:33:01 +0100

    obiektowosc poprawki i dodnie funkcjonalnosco odwracania jak nie ma pary i usuwania jak jest para

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 var Memo = function(rows, columns) {
     this.rows = rows;
     this.columns = columns;
+    lastClickedMemoItem = null;
 
 }
 
@@ -20,17 +21,18 @@ Memo.prototype.buildMemo = function() {
     for (let i = 0; i < this.rows; i++) {
         for (let j = 0; j < this.columns; j++) {
             memo.appendChild(new MemoItem("memoItem", "memo_" + i + "_" + j,
-                    "http://icons.iconarchive.com/icons/dapino/girl-in-a-bunny-suit/256/girl-bunny-question-icon.png")
+                    "http://icons.iconarchive.com/icons/dapino/girl-in-a-bunny-suit/256/girl-bunny-question-icon.png", this)
                 .generateMemoCardDiv());
         }
-        memo.appendChild(new MemoItem("empty", "", "").generateMemoCardDiv());
+        memo.appendChild(new MemoItem("empty", "", "", this).generateMemoCardDiv());
     }
 }
 
-var MemoItem = function(class_, id, url) {
+var MemoItem = function(class_, id, url, memo) {
     this.class_ = class_;
     this.id = id;
     this.url = url;
+    this.memo = memo;
 }
 
 MemoItem.prototype.generateMemoCardDiv = function() {
@@ -39,7 +41,7 @@ MemoItem.prototype.generateMemoCardDiv = function() {
     if (this.class_ == "memoItem") {
         newDiv.dataset.url = this.url;
         newDiv.id = this.id;
-        newDiv.addEventListener("click", this.onMemoCardClick);
+        newDiv.addEventListener("click", () => { this.onMemoCardClick(this) });
         newDiv.appendChild(this.generateImgForDiv());
     }
     return newDiv;
@@ -52,10 +54,47 @@ MemoItem.prototype.generateImgForDiv = function() {
     return img;
 }
 
-MemoItem.prototype.onMemoCardClick = function(event) {
-    event.target.querySelector(".hidden").style.visibility = "visible";
+MemoItem.prototype.onMemoCardClick = function(clickedMemoItem) {
+    if (clickedMemoItem.memo.lastClickedMemoItem == null) {
+        clickedMemoItem.memo.lastClickedMemoItem = clickedMemoItem;
+        event.target.querySelector(".hidden").style.visibility = "visible";
+        //clickedMemoItem.revert();
+    } else {
+        event.target.querySelector(".hidden").style.visibility = "visible";
+        setTimeout(() => {}, 5000);
+        if (clickedMemoItem.compare(clickedMemoItem.memo.lastClickedMemoItem)) {
+            clickedMemoItem.disable();
+            clickedMemoItem.memo.lastClickedMemoItem.disable();
+        } else {
+            clickedMemoItem.memo.lastClickedMemoItem.revert();
+            clickedMemoItem.revert();
+        }
+        clickedMemoItem.memo.lastClickedMemoItem = null;
+    }
 }
 
+MemoItem.prototype.revert = function() {
+    let memoItemElem = document.getElementById(this.id).querySelector(".hidden");
+    //  console.log(document.querySelector("#memo_3_2").querySelector(".hidden").style);
+    if (memoItemElem.style.visibility == "hidden") {
+        memoItemElem.style.visibility = "visible";
+    } else
+        memoItemElem.style.visibility = "hidden";
+}
+
+MemoItem.prototype.disable = function() {
+    document.getElementById(this.id).style.visibility = "hidden";
+    document.getElementById(this.id).style.visibility = "hidden";
+    this.revert();
+}
+
+MemoItem.prototype.compare = function(memoItem) {
+    return this.url === memoItem.url;
+}
+
+
+
+//future use
 function generateArrayOfPairsInRandomOrder(numberOfUniqueElements) {
     let left = generateArrayOfUniqueNumbers(numberOfUniqueElements);
     let right = generateArrayOfUniqueNumbers(numberOfUniqueElements);
